e7ae6023183b74424d03d9546eca9a402ab1b3c0
Leanne Teeter <lrteeter@gmail.com>
2016-01-12 15:21:33 -0800

    Update recursion_exercises.js

--- recursion_exercises.js
+++ recursion_exercises.js
@@ -5,11 +5,7 @@ function range(start, end) {
     return arr;
   }
   else {
-    console.log("start " + start);
-    console.log("end " + end);
     range(start, end - 1).push(end);
   }
   return arr;
 }
-
-console.log(range(4, 9));
